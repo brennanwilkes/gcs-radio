d2e68c7bddbec6340e16d01ed07cdb755af2fc34
Brennan Wilkes <brennan@codexwilkes.com>
2021-01-30 19:55:03 -0800

    feat: Typescript setup

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
 	mode: "development",
 	devtool: "source-map",
-	entry: "./src/frontend/index.js",
+	entry: "./src/frontend/index.ts",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "..", "public-frontend")
@@ -27,8 +27,16 @@ module.exports = {
 				use: {
 					loader: "url-loader?limit=8192"
 				}
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
 			}
 		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ],
 	},
 	externals: {
 		jquery: "jQuery"
