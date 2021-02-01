451c3210a4460cf033202183d9059be7afb17183
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-01 15:13:58 -0800

    feat(tests): Util spec unit test

---
+++ src/backend/util.spec.ts
@@ -0,0 +1,13 @@
+import { print, CONFIG } from './util';
+
+test('Print integrity', () => {
+	console.log = jest.fn()
+	CONFIG.verbose = true
+	print('TEST')
+	expect(console.log).toHaveBeenCalledWith('TEST')
+
+	console.log = jest.fn()
+	CONFIG.verbose = false
+	print('TEST')
+	expect(console.log).not.toHaveBeenCalledWith('TEST')
+})
