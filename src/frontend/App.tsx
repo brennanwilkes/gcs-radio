6a114c15948bee07152943dc0b717de58e4905e3
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-01 11:28:30 -0800

    feat: Initial react boilerplate

---
+++ src/frontend/App.tsx
@@ -0,0 +1,26 @@
+// Brennan Wilkes
+
+// Imports
+import * as React from "react";
+import axios from "axios";
+
+/**
+ * Main GCS Radio App Component.
+ * Renders out main route level sub-components,
+ * manages all global state information, and links
+ * subcomponents together using callback methods
+ * @class
+ * @extends React.Component
+ */
+class App extends React.Component {
+
+	/**
+	 *
+	 */
+	render () {
+		return <>
+			<h2>GCS Radio</h2>
+		</>;
+	}
+}
+export default App;
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
 	mode: "development",
 	devtool: "source-map",
-	entry: "./src/frontend/index.ts",
+	entry: "./src/frontend/index.tsx",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "..", "public-frontend")
