f90a3f3a8b04bfee5bfaf99ab504ad26088cb31a
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-01 14:26:20 -0800

    feat: Server abstraction object

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,27 +1,6 @@
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
+// Brennan Wilkes
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+import RadioServer from "./server";
 
-app.use(express.json());
-
-// support encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Static routing for public files
-app.use("/", express.static(path.join(__dirname, "..", "public-frontend")));
-
-// 404 messages
-app.get("*", (req, res) => {
-	console.log("Received invalid GET request for", req.url);
-
-	res.writeHead(404, { "Content-Type": "text/html" });
-	res.write("<h1>404: If you see this, it's already too late!</h1>");
-	res.end();
-});
-
-app.listen(PORT, () => {
-	console.log(`server started at http://localhost:${PORT}`);
-});
+const server = new RadioServer();
+server.start();
---
+++ src/backend/util.ts
@@ -0,0 +1,13 @@
+
+// Store all backend config vars here
+import CONFIG from "./gcs.config.json";
+export {CONFIG};
+
+/**
+	Prints to stdout if verbose config mode is set
+	@param {string[]} content Content strings to print
+	@memberof backend
+*/
+export const print = function(...content: string[] ): void{
+	if (CONFIG.verbose) console.log(...content);
+};
