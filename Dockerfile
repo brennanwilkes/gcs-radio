9351a5d788f6f83bcc85528279762b47e7b2b5e1
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-06 14:46:51 -0800

    fix: Installed python2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:12-alpine
 ARG DB_CONNECTION=$DB_CONNECTION
 
+RUN apk add --no-cache python py-pip
 RUN apk add  --no-cache ffmpeg
 
 WORKDIR /usr/src/app
--- src/backend/youtube/downloadURLinfo.ts
+++ src/backend/youtube/downloadURLinfo.ts
@@ -5,20 +5,22 @@ export default function (url: string): Promise<VideoData> {
 	return new Promise((resolve, reject) => {
 		youtubedl.getInfo(url, [], function (err, info: any) {
 			if (err) {
+				console.error(err);
 				reject(err);
+			} else {
+				resolve(new VideoDataObj(
+					info.upload_date ?? "20200202",
+					info.duration ?? "0:00",
+					info.fulltitle ?? "Unknown",
+					info.album ?? "Unknown",
+					info.title ?? "Unknown",
+					info.id ?? "Unknown",
+					info.tags ?? [],
+					info.track ?? "Unknown",
+					info.thumbnails ?? [],
+					info.artist ?? "Unknown"
+				));
 			}
-			resolve(new VideoDataObj(
-				info.upload_date ?? "20200202",
-				info.duration ?? "0:00",
-				info.fulltitle ?? "Unknown",
-				info.album ?? "Unknown",
-				info.title ?? "Unknown",
-				info.id ?? "Unknown",
-				info.tags ?? [],
-				info.track ?? "Unknown",
-				info.thumbnails ?? [],
-				info.artist ?? "Unknown"
-			));
 		});
 	});
 }
