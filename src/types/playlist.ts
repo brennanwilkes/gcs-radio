3690d0ac91360915c163bf0b69f9ddb718ae93a2
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-17 11:25:45 -0800

    feat: Initial playlist type object

---
+++ src/types/playlist.ts
@@ -0,0 +1,40 @@
+import { Song } from "./song";
+import axios from "axios";
+
+export interface Playlist{
+	songs: Song[],
+	add(song: Song): Playlist,
+	render(songResponseCallback?: (song: Song) => void): Promise<Playlist>,
+}
+
+export class PlaylistObj implements Playlist {
+	songs: Song[];
+	constructor () {
+		this.songs = [];
+	}
+
+	add (song: Song): PlaylistObj {
+		this.songs.push(song);
+		return this;
+	}
+
+	render (songResponseCallback?: (song: Song) => void) {
+		return new Promise<PlaylistObj>((resolve, reject) => {
+			const requests = this.songs.map(song => {
+				return axios.post(`/api/v1/songs?youtubeId=${encodeURIComponent(song.youtubeId)}&spotifyId=${encodeURIComponent(song.spotifyId)}`).then(resp => {
+					if (songResponseCallback) {
+						songResponseCallback(resp.data.songs[0]);
+					}
+					return resp;
+				});
+			});
+
+			Promise.all(requests).then(responses => {
+				const converted: Song[] = responses.map(responses => responses.data.songs);
+				this.songs = converted;
+
+				resolve(this);
+			}).catch(reject);
+		});
+	}
+}
