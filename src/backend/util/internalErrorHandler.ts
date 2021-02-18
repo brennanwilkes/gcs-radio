052659672c573333f31bf2d35242c28893c98183
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-18 13:13:18 -0800

    fix: Spotify result errors

--- src/backend/util/internalErrorHandler.ts
+++ src/backend/util/internalErrorHandler.ts
@@ -3,7 +3,7 @@ import { ErrorFromException, Error } from "../../types/error";
 import { print } from "./util";
 
 export default (req: Request, res: Response) => (err: string): void => {
-	const error: Error = new ErrorFromException(err, req.originalUrl);
+	const error: Error = new ErrorFromException(String(err), req.originalUrl);
 	print("Internal Error:");
 	print(JSON.stringify(error, null, 4));
 	res.status(500).json({
