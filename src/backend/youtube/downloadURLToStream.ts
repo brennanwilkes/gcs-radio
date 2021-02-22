b951a4adc4930b6abcba3c7e64084f312f800ee3
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-21 18:46:54 -0800

    feat: Added canadian proxy server

--- src/backend/youtube/downloadURLToStream.ts
+++ src/backend/youtube/downloadURLToStream.ts
@@ -1,13 +1,21 @@
 import ytdl from "ytdl-core";
 import dummyPipe from "../util/dummyPipe";
 import { Transform } from "stream";
+import HttpsProxyAgent from "https-proxy-agent";
+
+const proxy = "http://64.235.204.107:3128";
+const agent = HttpsProxyAgent(proxy);
 
 export default function (url: string): Transform {
 	const dummy = dummyPipe();
-	ytdl(url, { quality: "highestaudio" })
+	ytdl(url, {
+		quality: "highestaudio",
+		requestOptions: { agent }
+	})
 		.on("error", err => {
 			console.error(`Failed to download ${url}`);
 			console.error(err);
 		}).pipe(dummy);
 	return dummy;
 }
+// 64.235.204.107	3128
