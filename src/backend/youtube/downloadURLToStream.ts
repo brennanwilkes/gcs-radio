57af2210bb8cf65c7469aa933b606b4c746b1395
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-21 18:58:53 -0800

    chore: Removed proxy

--- src/backend/youtube/downloadURLToStream.ts
+++ src/backend/youtube/downloadURLToStream.ts
@@ -1,16 +1,16 @@
 import ytdl from "ytdl-core";
 import dummyPipe from "../util/dummyPipe";
 import { Transform } from "stream";
-import HttpsProxyAgent from "https-proxy-agent";
+// import HttpsProxyAgent from "https-proxy-agent";
 
-const proxy = "http://64.235.204.107:3128";
-const agent = HttpsProxyAgent(proxy);
+// const proxy = "http://64.235.204.107:3128";
+// const agent = HttpsProxyAgent(proxy);
 
 export default function (url: string): Transform {
 	const dummy = dummyPipe();
 	ytdl(url, {
-		quality: "highestaudio",
-		requestOptions: { agent }
+		quality: "highestaudio"
+		//		requestOptions: { agent }
 	})
 		.on("error", err => {
 			console.error(`Failed to download ${url}`);
