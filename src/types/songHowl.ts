e276d9f24bcec9fc6f55870bb844848c759b2840
Brennan Wilkes <brennan@codexwilkes.com>
2021-02-27 10:05:55 -0800

    fix: HTML5 audio on mobile IOS

--- src/types/songHowl.ts
+++ src/types/songHowl.ts
@@ -2,14 +2,25 @@ import { Howl } from "howler";
 import { Song } from "./song";
 import { VoiceLineRender } from "./voiceLine";
 
+const iOS = (): boolean => [
+	"iPad Simulator",
+	"iPhone Simulator",
+	"iPod Simulator",
+	"iPad",
+	"iPhone",
+	"iPod"
+].includes(navigator.platform) ||
+	// iPad on iOS 13 detection
+	(navigator.userAgent.includes("Mac") && "ontouchend" in document);
+
 export default class SongHowl extends Howl {
 	constructor (audio: Song | VoiceLineRender) {
 		super({
 			src: `/api/audio/${audio?.audioId}`,
 			format: ["mp3"],
 			autoplay: false,
-			preload: false
-			//			html5: true
+			preload: false,
+			html5: iOS()
 		});
 	}
 }
