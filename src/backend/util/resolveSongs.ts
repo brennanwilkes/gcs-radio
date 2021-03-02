bc128fbdf4ceb8f6d5637204c8b7b1692ae4417d
Brennan Wilkes <brennan@codexwilkes.com>
2021-03-01 21:27:38 -0800

    feat: Resolve songs unit tests

--- src/backend/util/resolveSongs.ts
+++ src/backend/util/resolveSongs.ts
@@ -8,22 +8,19 @@ export default function (spotifyResults: SpotifyResult[], searchAttempts = 10): 
 	return new Promise<Song[]>((resolve, reject) => {
 		const songResults: Promise<(Song | void)>[] = spotifyResults.map(async (spotifySong, _songNumber) => {
 			print(`Querying youtube for ${spotifySong.title} by ${spotifySong.artist}`);
-			const youtubeIds = await searchYoutubeSimple(`song ${spotifySong.title} by ${spotifySong.artist} official`, searchAttempts);
+			const youtubeIds = await searchYoutubeSimple(`song ${spotifySong.title} by ${spotifySong.artist} official`, searchAttempts).catch(reject);
+			if (youtubeIds) {
+				for (let i = 0; i < youtubeIds.length; i++) {
+					print(`Querying youtube for ${youtubeIds[i]} metadata`);
+					const youtubeDetails = await searchYoutubeDetailed(youtubeIds[i]).catch(reject);
 
-			for (let i = 0; i < youtubeIds.length; i++) {
-				print(`Querying youtube for ${youtubeIds[i]} metadata`);
-				const youtubeDetails = await searchYoutubeDetailed(youtubeIds[i]).catch(reject);
-
-				if (youtubeDetails && youtubeDetails.formats.length < 1) {
-					print(`No youtube formats found for ${youtubeDetails.youtubeTitle} (${spotifySong.title})`);
+					if (youtubeDetails && youtubeDetails.formats.length > 0 && resultMatches(spotifySong, youtubeDetails)) {
+						print(`Success finding match on try ${i + 1} for ${spotifySong.title}`);
+						return new SongFromSearch(youtubeDetails, spotifySong);
+					}
 				}
-
-				if (youtubeDetails && youtubeDetails.formats.length > 0 && resultMatches(spotifySong, youtubeDetails)) {
-					print(`Success finding match on try ${i + 1} for ${spotifySong.title}`);
-					return new SongFromSearch(youtubeDetails, spotifySong);
-				}
+				print(`Failure finding match for ${spotifySong.title}`);
 			}
-			print(`Failure finding match for ${spotifySong.title}`);
 		});
 
 		Promise.all(songResults).then(results => {
