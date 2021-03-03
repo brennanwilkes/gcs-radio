2009d77d4d560bf0d3aa3c7fd8d9e91898a542ae
Brennan Wilkes <brennan@codexwilkes.com>
2021-03-03 15:24:25 -0800

    feat: Unit tests for error handlers added in 931b6eb

---
+++ src/backend/util/invalidLoginErrorHandler.spec.ts
@@ -0,0 +1,29 @@
+import invalidLoginErrorHandler from "./invalidLoginErrorHandler";
+
+const req = {
+	originalUrl: "URL"
+};
+
+const res = {
+	status: (status: number) => {
+		expect(status).toBe(401);
+		return {
+			json: (json: any) => {
+				expect(json).toMatchObject({
+					errors: expect.any(Array)
+				});
+				expect(json.errors.length).toBe(1);
+				return {
+					end: () => {
+						expect(true).toBeTruthy();
+					}
+				};
+			}
+		};
+	}
+}
+
+test("Generates correct JSON response", () => {
+	expect.assertions(4);
+	invalidLoginErrorHandler(req as any, res as any)("ERROR");
+});
