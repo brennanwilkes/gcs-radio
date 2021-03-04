81be5d7bcde7e3705e0bbc33d5c0dbd4fef7a103
Brennan Wilkes <brennan@codexwilkes.com>
2021-03-03 19:06:05 -0800

    fix: Updated google client certs

--- src/backend/controllers/oauthGoogleController.ts
+++ src/backend/controllers/oauthGoogleController.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from "express";
 import { CONFIG } from "../util/util";
 import { google } from "googleapis";
-import fs from "fs";
 import internalErrorHandler from "../util/internalErrorHandler";
 
 const OAuth2 = google.auth.OAuth2;
 
-const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS ? JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS as string, "utf8")) : undefined;
-
 const redirectToGoogle = (req: Request, res: Response): void => {
 	if (!process.env.GOOGLE_PROJECT) {
 		internalErrorHandler(req, res)("Google project not set");
-	} else if (!GOOGLE_APPLICATION_CREDENTIALS || !GOOGLE_APPLICATION_CREDENTIALS.client_id || !GOOGLE_APPLICATION_CREDENTIALS.private_key) {
-		internalErrorHandler(req, res)("Google application credentials not set");
+	}
+	if (!process.env.GOOGLE_CLIENT_ID) {
+		internalErrorHandler(req, res)("Google OAUTH ID not set");
+	}
+	if (!process.env.GOOGLE_CLIENT_SECRET) {
+		internalErrorHandler(req, res)("Google OAUTH secret not set");
 	} else {
 		const oauth2Client = new OAuth2(
-			GOOGLE_APPLICATION_CREDENTIALS.client_id,
-			GOOGLE_APPLICATION_CREDENTIALS.private_key,
+			process.env.GOOGLE_CLIENT_ID as string,
+			process.env.GOOGLE_CLIENT_SECRET as string,
 			`${(req.protocol + "://" + req.get("host") + req.originalUrl).replace(/\/$/, "")}/callback`
 		);
 
