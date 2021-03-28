dd581ede6f7bba256e6baad07a87f4950f75a5f9
Brennan Wilkes <brennan@codexwilkes.com>
2021-03-27 20:02:01 -0700

    feat: Redone landing page

--- src/frontend/Landing/Landing.tsx
+++ src/frontend/Landing/Landing.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./landing.css";
+import "./landing.scss";
 
 
 interface IProps {}
@@ -16,11 +16,11 @@ export default class Landing extends React.Component<IProps, IState> {
 			<div className="Landing">
 				<h1>GCS Radio</h1>
 				<div className="container-fluid">
-					<a href="./browser" className="btn btn-info row col-12 p-1">BROWSE PLAYLISTS</a>
-					<a href="./builder" className="btn btn-success row col-12 p-1">BUILD PLAYLIST</a>
+					<a href="./browser" className="btn btn-gcs-faded row col-12 p-1">BROWSE PLAYLISTS</a>
+					<a href="./builder" className="btn btn-gcs-bright row col-12 p-1">BUILD PLAYLIST</a>
 					<div className="row my-2">
 						<a href="./login"><h6 className="col-4">Login</h6></a>
-						<h6 className="mx-n2 text-primary">|</h6>
+						<h6 className="mx-n2 text-gcs-alpine">|</h6>
 						<a href="./login?signup=1"><h6 className="col-4">Sign Up</h6></a>
 					</div>
 				</div>
