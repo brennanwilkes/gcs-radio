c85022a0a70d142263eed23ff069750b29d59c0f
Brennan Wilkes <brennan@codexwilkes.com>
2021-04-02 19:56:57 -0700

    feat: Landing page updates

--- src/frontend/Landing/Landing.tsx
+++ src/frontend/Landing/Landing.tsx
@@ -8,6 +8,11 @@ const Build = () => <>{useTranslation("common").t("landing.build")}</>
 const Login = () => <>{useTranslation("common").t("landing.login")}</>
 const SignUp = () => <>{useTranslation("common").t("landing.signup")}</>
 
+const Welcome1 = () => <>{useTranslation("common").t("landing.welcome1")}</>
+const Welcome2 = () => <>{useTranslation("common").t("landing.welcome2")}</>
+const Welcome3 = () => <>{useTranslation("common").t("landing.welcome3")}</>
+const Welcome4 = () => <>{useTranslation("common").t("landing.welcome4")}</>
+
 
 interface IProps {}
 interface IState {}
@@ -24,11 +29,20 @@ export default class Landing extends React.Component<IProps, IState> {
 				<div className="container-fluid">
 					<a href="./browser" className="btn btn-gcs-faded row col-12 p-1"><Browse /></a>
 					<a href="./builder" className="btn btn-gcs-bright row col-12 p-1"><Build /></a>
-					<div className="row my-2">
+					<div className="row mt-2 mb-1">
 						<a href="./login"><h6 className="col-4"><Login /></h6></a>
 						<h6 className="mx-n2 text-gcs-alpine">|</h6>
 						<a href="./login?signup=1"><h6 className="col-4"><SignUp /></h6></a>
 					</div>
+					<p className="mt-md-5 mb-2 text-gcs-alpine">
+						<Welcome1 /><a className="text-gcs-bright mx-1" target="_blank" href="https://github.com/brennanwilkes/gcs-radio">GitHub</a>
+					</p>
+					<p className="mt-md-2 mb-2 text-gcs-alpine">
+						<Welcome2 /><a className="text-gcs-bright mx-1" target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+					</p>
+					<p className="mt-md-2 mb-2 text-gcs-alpine">
+						<Welcome3 /><a className="text-gcs-bright mx-1" target="_blank" href="mailto:brennan@codexwilkes.com">brennan@codexwilkes.com</a><Welcome4 />
+					</p>
 				</div>
 			</div>
 		</>
