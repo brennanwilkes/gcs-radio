c1a659bdbd607494713ab79a6136917d6e41769c
Brennan Wilkes <brennan@codexwilkes.com>
2021-04-04 21:51:50 -0700

    feat: Added generate link for navbar

--- src/frontend/Navbar/Navbar.tsx
+++ src/frontend/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import {UserWithId} from "../../types/user";
 
 import {useTranslation} from "react-i18next";
 
-const Home = () => <>{useTranslation("common").t("nav.home")}</>
+//const Home = () => <>{useTranslation("common").t("nav.home")}</>
 const Build = () => <>{useTranslation("common").t("nav.build")}</>
 const Browse = () => <>{useTranslation("common").t("nav.browse")}</>
 const Login = () => <>{useTranslation("common").t("nav.login")}</>
@@ -61,7 +61,7 @@ export default class Navbar extends React.Component<IProps, IState> {
 	render(){
 
 		const navigation: NavItemData[] = [
-			{text: <Home />, href: ".."},
+			{text: <>Generate</>, href: "../generate"},
 			{text: <Build />, href: "../build"},
 			{text: <Browse />, href: "../browse"},
 			{text: (this.state.user ? <Profile /> : <Login />), href: `../${this.state.loggedIn ? "dashboard" : "login"}`},
