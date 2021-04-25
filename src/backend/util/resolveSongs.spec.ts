5e392d267c2e18eb2ff627aaa7c637ee00771882
Brennan Wilkes <brennan@codexwilkes.com>
2021-04-25 15:24:43 -0700

    fix: Resolve songs test

--- src/backend/util/resolveSongs.spec.ts
+++ src/backend/util/resolveSongs.spec.ts
@@ -1,8 +1,10 @@
 import resolveSongs from "./resolveSongs";
 import { CONFIG } from "./util";
 
-process.env.MATCH_WITH_YOUTUBE = "true";
-CONFIG.matchWithYoutube = true;
+process.env.MATCH_WITH_YOUTUBE = "false";
+CONFIG.matchWithYoutube = false;
+CONFIG.matchWithMusicKit = false;
+process.env.MATCH_WITH_MUSIC_KIT = "false";
 
 
 const validSong = {
@@ -18,21 +20,9 @@ const validSong = {
 	releaseDate: "2013-01-01",
 }
 
-const invalidSong = {
-	title: "DLFKJSLDKJ aljd asd930 dskasdfklj asdfkljfdsakl a",
-	artist: "asfklskld;skl;kld;kl dk ;ld kd ;lksd;lk adl k;sd",
-	album: "popopodapfo asdpfpao sfasdklfas;dkf",
-	duration: 100000000000,
-	explicit: false,
-	spotifyId: "7GDDxCiTyiQuC1kig8AqFH",
-	artistSpotifyId: "536BYVgOnRky0xjsPT96zl",
-	albumSpotifyId: "5Z5DIr86vr1F4ZJbRFuQ6Q",
-	thumbnailUrl: "https://i.scdn.co/image/ab67616d0000b273015d7007c3c0390a62c6f20b",
-	releaseDate: "2013-01-01",
-}
 
 test("Resolves a valid song", done => {
-	expect.assertions(10);
+	expect.assertions(5);
 	resolveSongs([validSong]).then(res => {
 		expect(res.length).toBe(1);
 		expect(res[0].title).toBe(validSong.title);
@@ -42,12 +32,4 @@ test("Resolves a valid song", done => {
 		done();
 	});
 
-	resolveSongs([validSong]).then(res => {
-		expect(res.length).toBe(1);
-		expect(res[0].title).toBe(validSong.title);
-		expect(res[0].artist).toBe(validSong.artist);
-		expect(res[0].album).toBe(validSong.album);
-		expect(res[0].duration).toBe(validSong.duration);
-		done();
-	});
 });
