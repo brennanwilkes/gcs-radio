4b480b2cf89ed155dc5324d8422a684d202ae08f
Brennan Wilkes <brennan@codexwilkes.com>
2021-05-01 12:44:00 -0700

    chore: More CI/CD Adjustments

--- src/backend/musicKit/generateDeveloperToken.ts
+++ src/backend/musicKit/generateDeveloperToken.ts
@@ -1,6 +1,5 @@
 import { CONFIG } from "../util/util";
 import jwt from "jsonwebtoken";
-import fs from "fs";
 
 export default (): Promise<string> => new Promise<string>((resolve, reject) => {
 	if (!CONFIG.musicKitKeyId) {
@@ -10,20 +9,14 @@ export default (): Promise<string> => new Promise<string>((resolve, reject) => {
 	} else if (!CONFIG.musicKitSecret) {
 		reject(new Error("MusicKit Secret not set"));
 	} else {
-		fs.readFile(CONFIG.musicKitSecret, (err: Error | null, data: Buffer): void => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(jwt.sign({}, data.toString(), {
-					algorithm: "ES256",
-					expiresIn: CONFIG.musicKitTokenExpiry,
-					issuer: CONFIG.musicKitTeamId,
-					header: {
-						alg: "ES256",
-						kid: CONFIG.musicKitKeyId
-					}
-				}));
+		resolve(jwt.sign({}, CONFIG.musicKitSecret, {
+			algorithm: "ES256",
+			expiresIn: CONFIG.musicKitTokenExpiry,
+			issuer: CONFIG.musicKitTeamId,
+			header: {
+				alg: "ES256",
+				kid: CONFIG.musicKitKeyId
 			}
-		});
+		}));
 	}
 });
