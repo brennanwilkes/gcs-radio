431afa5af841f9163cdea7b6a34b57890ed0b0a8
Brennan Wilkes <brennan@codexwilkes.com>
2021-05-01 13:59:56 -0700

    fix: undefined err

--- src/config/musicKit.ts
+++ src/config/musicKit.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 export default {
-	musicKitSecret: process.env.MUSIC_KIT_SECRET.replace(/\\n/gm, "\n"),
+	musicKitSecret: process.env.MUSIC_KIT_SECRET ? process.env.MUSIC_KIT_SECRET.replace(/\\n/gm, "\n") : process.env.MUSIC_KIT_SECRET,
 	musicKitTeamId: process.env.MUSIC_KIT_TEAM_ID,
 	musicKitKeyId: process.env.MUSIC_KIT_KEY_ID,
 	musicKitTokenExpiry: "1d"
