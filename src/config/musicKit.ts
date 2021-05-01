dde8b0f23f36bcd68f15a4bc192dda8edabac874
Brennan Wilkes <brennan@codexwilkes.com>
2021-05-01 13:49:21 -0700

    fix: New line escaping in secret

--- src/config/musicKit.ts
+++ src/config/musicKit.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 export default {
-	musicKitSecret: process.env.MUSIC_KIT_SECRET,
+	musicKitSecret: process.env.MUSIC_KIT_SECRET.replace(/\\n/gm, "\n"),
 	musicKitTeamId: process.env.MUSIC_KIT_TEAM_ID,
 	musicKitKeyId: process.env.MUSIC_KIT_KEY_ID,
 	musicKitTokenExpiry: "1d"
